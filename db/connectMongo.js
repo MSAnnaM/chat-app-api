af0f37d24c33c6f6e6d61e9299cb690d755a617b
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2024-08-26 21:56:23 +0300

    add mongo session

--- db/connectMongo.js
+++ db/connectMongo.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 const { MONGO_URL } = process.env;
 const conectMongo = async () => {
-    try {
-        await mongoose.connect(MONGO_URL);
-        console.log("Database connection successful");
-        
-    } catch (er) {
-        console.log(er);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connection successful");
+  } catch (er) {
+    console.log(er);
+    process.exit(1);
+  }
+};
 export default conectMongo;
