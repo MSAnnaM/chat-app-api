a3b245feb470450b3ddde98d720970e3bb43cae4
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2024-08-26 21:44:21 +0300

    fix

--- helpers/passport.js
+++ helpers/passport.js
@@ -19,8 +19,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // callbackURL: "/auth/google/callback",
-      callbackURL: 'https://chat-app-api-production-8dc6.up.railway.app/auth/google/callback',
+      callbackURL: "/auth/google/callback",
+      // callbackURL: 'https://chat-app-api-production-8dc6.up.railway.app/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
